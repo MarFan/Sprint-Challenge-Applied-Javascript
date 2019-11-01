457e25cbe4029aaeec0c53a1093890cfd9ae1b60
Mr. Jonah <jaitch@gmail.com>
2019-11-01 12:05:57 -0500

    MVP and some stretch

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,46 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+let myTemp = '98\u00B0';
+parent = document.querySelector('.header-container');
+
+window.navigator.geolocation.getCurrentPosition(
+    (pos) => {
+        myLat = pos.coords.latitude;
+        myLong = pos.coords.longitude;
+        axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&units=imperial&appid=c179ff3c121ab234c61e31416c48c4fd`)
+            .then(res => {
+                myTemp = `${Math.floor(res.data.main.temp)}\u00B0`;
+            })
+            .then(() => {
+                parent.appendChild(Header());
+            })
+            .catch(err => {
+                myTemp = '98\u00B0';
+                parent.appendChild(Header());
+            })
+    }
+)
+
+function Header() {
+
+    // create the header
+	const newHeader = document.createElement('div');
+	const dateSpan = document.createElement('span');
+	const headerTitle = document.createElement('h1')
+	const headerTemp = document.createElement('span');
+
+	newHeader.classList.add('header');
+	dateSpan.classList.add('date');
+
+	headerTemp.textContent = myTemp;
+    headerTitle.textContent = 'Lambda Times';
+    dateSpan.textContent = new Date().toLocaleDateString("en-US", {month: 'long', day: 'numeric', year: 'numeric'});
+
+	newHeader.appendChild(dateSpan);
+	newHeader.appendChild(headerTitle);
+	newHeader.appendChild(headerTemp);
+
+	return newHeader;
+
+}
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,36 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+	.then(res => {
+        res.data.topics.forEach(t  => {
+            tabItem = document.createElement('div');
+                tabItem.dataset.topic = t.split('.')[0];
+            tabItem.classList.add('tab');
+            tabItem.textContent = t;
+            document.querySelector('.topics').appendChild(tabItem);
+        })
+    })
+    .then(() => {
+        createTabEvents();
+    })
+
+
+function createTabEvents() {
+    const tabEvent = document.querySelectorAll('.tab').forEach(t => {
+        t.addEventListener('click', (e) => {
+            let selectedTopic = e.target.dataset.topic;
+            document.querySelectorAll('.card').forEach(card => {
+                if(card.dataset.topic !== selectedTopic){
+                    //card.classList.add('hideMe');
+                    TweenMax.to(card, .5, {autoAlpha: 0, display: 'none'})
+                }else{
+                    //card.classList.remove('hideMe');
+                    TweenMax.to(card, .5, {autoAlpha: 1, display: 'block'})
+                }
+            })
+        });
+    });
+}
+
